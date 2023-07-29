2b57ba36bd06ee763ef466dfb8254faf43076378
Dr.pickle <129784141+Drpickle-git@users.noreply.github.com>
2023-07-29 15:03:59 +0200

    Update Main.py
    Bug Fixes

--- Main.py
+++ Main.py
@@ -16,34 +16,53 @@ import webbrowser
 import requests
 import random
 
+#La vérification n'est pas réele, je trouvait juste ca cool. 
 print(gris, "Vérification du node.js en cours..")
+print(" ")
 time.sleep(3)
 print(gris, "Téléchargement des paquets en cours..")
+print(" ")
 time.sleep(4)
 print(vert, "Téléchargement terminé !")
+print(" ")
 time.sleep(2)
 print(vert, "Démarrage du processus de vérification en cours..")
+print(" ")
 time.sleep(3)
 print(vert, "Vérification de l'utilisateur en cours")
+print(" ")
 time.sleep(5)
 print(vert, "Utilisateur vérifié !")
+print(" ")
+print("Promis Le reste n'est pas long :) ")
+print(" ")
 time.sleep(2)
 print(bleu_foncé, "Fermeture du node.js en cours..")
+print(" ")
 time.sleep(5)
 print(bleu, "Ouverture du logiciel en cours..")
+print(" ")
 time.sleep(6)
 
 banner = """
- _   _  _____  _____ ______  _____          _____  _____  _   _  _____ ______   ___   _____  _____ ______ 
+ _   _  _____  _____ ______  _____          _____  _____  _   _  _____ ______   ___   _____  _____ ______
 | \ | ||_   _||_   _|| ___ \|  _  |        |  __ \|  ___|| \ | ||  ___|| ___ \ / _ \ |_   _||  _  || ___ \
 |  \| |  | |    | |  | |_/ /| | | | ______ | |  \/| |__  |  \| || |__  | |_/ // /_\ \  | |  | | | || |_/ /
-| . ` |  | |    | |  |    / | | | ||______|| | __ |  __| | . ` ||  __| |    / |  _  |  | |  | | | ||    / 
-| |\  | _| |_   | |  | |\ \ \ \_/ /        | |_\ \| |___ | |\  || |___ | |\ \ | | | |  | |  \ \_/ /| |\ \ 
+| . ` |  | |    | |  |    / | | | ||______|| | __ |  __| | . ` ||  __| |    / |  _  |  | |  | | | ||    /
+| |\  | _| |_   | |  | |\ \ \ \_/ /        | |_\ \| |___ | |\  || |___ | |\ \ | | | |  | |  \ \_/ /| |\ \
 \_| \_/ \___/   \_/  \_| \_| \___/          \____/\____/ \_| \_/\____/ \_| \_|\_| |_/  \_/   \___/ \_| \_|
                                                                                                           
 """.format(random.choice([bleu_foncé, vert, orange]))
 
-print(banner)
+#print(banner)
+print(" _   _  _____  _____ ______  _____          _____  _____  _   _  _____ ______   ___   _____  _____ ______  ")
+print("| \ | ||_   _||_   _|| ___ \|  _  |        |  __ \|  ___|| \ | ||  ___|| ___ \ / _ \ |_   _||  _  || ___ \ ")
+print("|  \| |  | |    | |  | |_/ /| | | | ______ | |  \/| |__  |  \| || |__  | |_/ // /_\ \  | |  | | | || |_/ / ")
+print("| . ` |  | |    | |  |    / | | | ||______|| | __ |  __| | . ` ||  __| |    / |  _  |  | |  | | | ||    /  ")
+print("| |\  | _| |_   | |  | |\ \ \ \_/ /        | |_\ \| |___ | |\  || |___ | |\ \ | | | |  | |  \ \_/ /| |\ \  ")
+print("\_| \_/ \___/   \_/  \_| \_| \___/          \____/\____/ \_| \_/\____/ \_| \_|\_| |_/  \_/   \___/ \_| \_| ")
+
+print(" ")
 
 num = input("Combien de codes souhaitez-vous vérifier ? ")
 
@@ -52,8 +71,12 @@ f = open("Nitro Codes .txt", "w", encoding='utf-8')
 print(gris, "Ouverture de node.js... ")
 time.sleep(3)
 print(gris, 'Téléchargement des paquets...')
+print(" ")
+print(" ####- - - - - - 40 Pourcents...")
+time.sleep(2)
+print(" ######## - - 80 Pourcents...")
 time.sleep(4)
-print(vert, "Téléchargés !")
+print(vert, "Paquets Téléchargés !")
 time.sleep(2)
 print(bleu_foncé, 'Fermeture du node.js')
 time.sleep(5)
